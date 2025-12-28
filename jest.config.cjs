f722368e3db6a197a0d18b0dc5b2a3ae7e9e9e02
andys8 <andys8@users.noreply.github.com>
2025-12-28 19:37:42 +0100

    fix: use jest.config.cjs and add .nojekyll

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,8 +1,8 @@
-export default {
+module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jest-environment-jsdom',
   transform: {
-    '^.+\.tsx?$': 'ts-jest',
+    '^.+\.(ts|tsx)$': 'ts-jest',
   },
   moduleNameMapper: {
     '\.(css|less|scss|sass)$': 'identity-obj-proxy',
